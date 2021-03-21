fabac1482dcd539f09617688625eed4145598374
Masataka-n <masataka.nakagawa@diverse-inc.com>
2021-03-21 20:24:13 +0900

    Initial Commit

---
+++ CleanArchitectureForSwiftUI/CleanArchitectureForSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanArchitectureForSwiftUIApp.swift
+//  CleanArchitectureForSwiftUI
+//
+//  Created by Masataka Nakagawa on 2021/03/21.
+//
+
+import SwiftUI
+
+@main
+struct CleanArchitectureForSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
